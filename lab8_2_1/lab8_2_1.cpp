a4529588e253a2d92823a87bd6e371e051a65bfe
KonstantinovIKM723a <alex7wrt@gmail.com>
2024-03-12 14:02:25 +0200

    Update

--- lab8_2_1/lab8_2_1.cpp
+++ lab8_2_1/lab8_2_1.cpp
@@ -1,20 +1,57 @@
-// lab8_2_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "windows.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int n = 20;
+    float a[n];
+
+    srand(time(NULL));
+
+    //Ініціалізуємо масив дійсних чисел довжиною n елементів значеннями від -10 до 10
+    for (int i = 0; i < n; i++) a[i] = float (rand() % 2001 - 1000)/100;
+
+    //Виводимо масив на екран
+    cout << "Елементи масиву: ";
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    cout << endl;
+
+    //а) кількість і добуток від'ємних елементів
+    int negative_N = 0;
+    float negative_P = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] < 0) {
+            negative_N += 1;
+            negative_P *= a[i];
+        }
+    }
+
+    //Виводимо результат на екран
+    cout << "кількість від'ємних елементів: " << negative_N << endl;
+    cout << "добуток від'ємних елементів: " << negative_P << endl;
+
+    //б) сума елементів масиву, розташованих між першим і другим додатними елементами.
+    int positive_N = 0;
+    float positive_S = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] > 0) {
+            if (positive_N == 1) {    
+                positive_S += a[i];
+                positive_N = 2;
+                break;
+            }
+            positive_N += 1;
+        }
+    }
+
+    //Виводимо результат на екран
+    if (positive_N == 2) {
+        cout << "Сума додатних елементів, розташованих між першим і другим додатними елементами: " << positive_S;
+    } else cout << "Додатних елементів менше двох";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
